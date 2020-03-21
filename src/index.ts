d574b202f09c3b7d1876a3cd5c6ebfaa741abfd0
KarthusLorin <karthuslorin@gmail.com>
2020-03-21 22:49:38 +0800

    complete

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
+import cookieSession from "cookie-session";
 import router from "./router";
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["aaa"],
+    maxAge: 24 * 60 * 60 * 1000
+  })
+);
 app.use(router);
 
 app.listen(7001, () => {
