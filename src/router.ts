352b1aa4b92e3328a92832275a32420a4c48029d
KarthusLorin <karthuslorin@gmail.com>
2020-03-21 21:33:51 +0800

    update

--- src/router.ts
+++ src/router.ts
@@ -5,15 +5,28 @@ import Analyzer from "./analyzer";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-  res.send("hello world");
+  res.send(`
+    <html>
+      <body>
+        <form method="post" action="/getData">
+          <input type="password" name="password" />
+          <button>提交</button>
+        </form>
+      </body>
+    </html>
+  `);
 });
 
-router.get("/getData", (req: Request, res: Response) => {
-  const secret = "x3b174jsx";
-  const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
-  const analyzer = Analyzer.getInstance();
-  new Crowller(url, analyzer);
-  res.send("getData Success!");
+router.post("/getData", (req: Request, res: Response) => {
+  if (req.body.password === "123") {
+    const secret = "x3b174jsx";
+    const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
+    const analyzer = Analyzer.getInstance();
+    new Crowller(url, analyzer);
+    res.send("getData Success!");
+  } else {
+    res.send("password error!");
+  }
 });
 
 export default router;
